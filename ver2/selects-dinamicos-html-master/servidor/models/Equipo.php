18ac5b8b141a760ea0d345f206824a46ec9dccc3
demitrimax <armandoaguilar1@yahoo.com>
2018-07-22 14:12:50 -0500

    Borrando basura

--- ver2/selects-dinamicos-html-master/servidor/models/Equipo.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Equipo extends ActiveRecord\Model
-{
-	public static $table_name = 'equipo';
-	public static $primary_key = 'id';
-}
-?>
--- ver2/selects-dinamicos-html-master/servidor/models/Jugador.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Jugador extends ActiveRecord\Model
-{
-	public static $table_name = 'jugador';
-	public static $primary_key = 'id';
-}
-?>
