db71fdba56ed5fe2aa8bc560eb2bde9e41ce5cb2
demitrimax <armandoaguilar1@yahoo.com>
2018-07-22 13:54:52 -0500

    modificaciones posteriores

---
+++ Connections/ResEquipos.php
@@ -0,0 +1,22 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_ResEquipos = "localhost";
+$database_ResEquipos = "id4491471_secoe";
+$username_ResEquipos = "id4491471_secoeuser";
+$password_ResEquipos = "secoeweb";
+$ResEquipos =  mysqli_connect($hostname_ResEquipos, $username_ResEquipos, $password_ResEquipos) or trigger_error(mysqli_error($ResEquipos),E_USER_ERROR); 
+
+function mysqli_result($res,$row=0,$col=0){ 
+    $numrows = mysqli_num_rows($res); 
+    if ($numrows && $row <= ($numrows-1) && $row >=0){
+        mysqli_data_seek($res,$row);
+        $resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
+        if (isset($resrow[$col])){
+            return $resrow[$col];
+        }
+    }
+    return false;
+}
+?>
